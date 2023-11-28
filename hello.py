2a9b92042c4714d607b4e68f667563406c630420
MarcG02 <marcgalaxy02@gmail.com>
2023-11-28 16:53:05 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/formulari')
+def formulari_get():
+    # mostrem el formulari
+    return """
+    <form method='post'>
+        Introdueix el teu nom:
+        <input name='nom' type='text' />
+        <br>
+        <input type='submit'>
+    </form>
+    """
+ 
+@app.route('/formulari', methods=['POST'])
+def formulari_post():
+    # processem les dades del formulari
+    nom = request.form["nom"]
+    return "Salut, {}".format(nom)
